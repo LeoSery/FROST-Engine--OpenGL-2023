128f37cad22414d51f5f57288d63ae3687f159ed
Séry Léo <74504632+LeoSery@users.noreply.github.com>
2023-12-05 16:42:46 +0100

    Project: Added solution configuration, project dependencies, directories and links for builds and game engine libraries.

---
+++ FROST/FrostEngine/Includes/Core/Devices/Graphics/WindowManager.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace Frost::HAL::Core
+{
+	class WindowManager
+	{
+
+	};
+}
---
+++ FROST/SnowDeformationDemo/main.cpp
@@ -0,0 +1,10 @@
+#include <Core/Devices/Graphics/WindowManager.h>
+
+using namespace Frost::HAL::Core;
+
+int main(int argc, char** argv)
+{
+	WindowManager windowManager;
+
+	return 0;
+}
